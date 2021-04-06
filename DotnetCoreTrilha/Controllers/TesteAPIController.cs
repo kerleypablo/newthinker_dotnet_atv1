7a5da90137796ab474a0ddb1c896a867df70fafa
kerley pablo <kerley.pablo@hotmail.com>
2021-04-06 17:58:29 -0300

    segundo comite

---
+++ DotnetCoreTrilha/Controllers/TesteAPIController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetCoreTrilha.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TesteAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("hellow wolrd");
+        }
+
+
+    }
+}
